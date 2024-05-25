ac63d1cb9ea76ae291cf5b1c6c06e33ac168c3cd
daenvngucii <120397126+daenv@users.noreply.github.com>
2024-05-25 17:39:08 +0700

    feat: Add course progress entity interface

---
+++ src/entities/interfaces/courseProgress.entity.interface.ts
@@ -0,0 +1,8 @@
+import { IBaseEntity } from './base.entity.interface';
+
+export interface ICourseProgress extends IBaseEntity {
+   completedLessons: boolean;
+   completedQuizzes: boolean;
+   progressPercentage: number;
+   lastAccessedAt: Date;
+}
