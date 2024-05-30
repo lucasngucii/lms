8670e18c377ceafd6230b4841024a0097ce23448
daenv <daenvle69@gmail.com>
2024-05-31 00:27:59 +0700

    feat: Add AccountEntity
    This commit adds the AccountEntity class, which represents the account entity in the database. It includes columns for username, email, password, isVerified, lastLogin, and relationships with other entities such as roles, sessions, keyToken, loginHistory, detail, enrollments, and courses.
    
    Recent user commits:
    - chore: Update console log message in main.ts
    - chore: Update npm dependencies to latest versions
    - feat: Add course tag entity interface
    - feat: Add coupon entity interface
    - feat: Add quiz question entity interface
    - feat: Add properties to lesson entity interface
    - feat: Add instructor detail entity interface
    - feat: Add course feedback entity interface
    - feat: Add course certificate entity interface
    - feat: Add category entity interface

---
+++ src/entities/account.entity.ts
@@ -0,0 +1,64 @@
+import {
+   Column,
+   Entity,
+   Index,
+   JoinColumn,
+   JoinTable,
+   ManyToMany,
+   OneToMany,
+   OneToOne,
+} from 'typeorm';
+import { BaseEntity } from './bases';
+import { IAccountEntity } from './interfaces';
+import { RoleEntity } from './role.entity';
+import { SessionEntity } from './session.entity';
+import { KeyTokenEntity } from './keyToken.entity';
+import { LoginHistoryEntity } from './loginHistory.entity';
+import { DetailAccountEntity } from './detailAccount.entity';
+import { EnrollmentEntity } from './enrollment.entity';
+import { CourseEntity } from './course.entity';
+
+@Entity({ name: 'account' })
+export class AccountEntity extends BaseEntity<AccountEntity> implements IAccountEntity {
+   @Index('IDX_ACCOUNT_USERNAME', { unique: true })
+   @Column({ type: 'varchar', unique: true })
+   username: string;
+
+   @Index('IDX_ACCOUNT_EMAIL', { unique: true })
+   @Column({ type: 'varchar', name: 'email' })
+   email: string;
+
+   @Column({ name: 'password', type: 'varchar' })
+   password: string;
+
+   @Column({ name: 'is_verified', type: 'boolean', default: false })
+   isVerified: boolean;
+
+   @Column({ name: 'last_login', type: 'timestamptz', default: null })
+   lastLogin: Date;
+
+   @ManyToMany(() => RoleEntity, { cascade: true })
+   @JoinTable()
+   roles: RoleEntity[];
+
+   @OneToMany(() => SessionEntity, (session) => session.account)
+   sessions: SessionEntity[];
+
+   @ManyToMany(() => KeyTokenEntity, { cascade: true })
+   @JoinTable()
+   keyToken: KeyTokenEntity[];
+
+   @OneToMany(() => LoginHistoryEntity, (history) => history.account)
+   loginHistory: LoginHistoryEntity[];
+
+   @OneToOne(() => DetailAccountEntity, { cascade: true })
+   @JoinColumn()
+   detail: DetailAccountEntity;
+
+   @OneToMany(() => EnrollmentEntity, (enrollment) => enrollment.account)
+   enrollments: EnrollmentEntity[];
+
+   @OneToMany(()=> CourseEntity,  course => course.instructor)
+   courses: CourseEntity[];
+
+}
