29eb645e7f0f2bc2e58754db18aef8619fe687af
daenv <daenvle69@gmail.com>
2024-05-31 00:31:00 +0700

    feat: Add LoginHistoryEntity
    This commit adds the LoginHistoryEntity class, which represents a login history record in the database. It includes columns for login time, login method, IP address, user agent, and a relationship with the AccountEntity.
    
    Recent user commits:
    - chore: Update console log message in main.ts
    - chore: Update npm dependencies to latest versions
    - feat: Add course tag entity interface
    - feat: Add coupon entity interface
    - feat: Add quiz question entity interface
    - feat: Add properties to lesson entity interface
    - feat: Add instructor detail entity interface
    - feat: Add course feedback entity interface
    - feat: Add course certificate entity interface
    - feat: Add category entity interface

---
+++ src/entities/loginHistory.entity.ts
@@ -0,0 +1,23 @@
+import { Column, CreateDateColumn, Entity, ManyToOne } from 'typeorm';
+
+import { AccountEntity } from './account.entity';
+import { BaseEntity } from './bases';
+import { ILoginHistory } from './interfaces';
+
+@Entity({ name: 'login_history' })
+export class LoginHistoryEntity extends BaseEntity<LoginHistoryEntity> implements ILoginHistory {
+   @CreateDateColumn({ name: 'login_time' })
+   loginTime: Date;
+
+   @Column({ type: 'varchar', name: 'login_method' })
+   loginMethod: string;
+
+   @Column({ type: 'varchar', name: 'ip_address' })
+   ipAddress: string;
+
+   @Column({ type: 'varchar', name: 'user_agent' })
+   userAgent: string;
+
+   @ManyToOne(() => AccountEntity, (account) => account.loginHistory)
+   account: AccountEntity;
+}
