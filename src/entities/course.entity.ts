63abdbaaca6459d3f965cfa278b0b57e29e586f3
daenv <daenvle69@gmail.com>
2024-05-31 00:28:21 +0700

    feat: Add CourseEntity
    This commit adds the CourseEntity class, which represents a course in the database. It includes columns for title, description, language, price, level, discount, category, thumbnailUrl, and relationships with other entities such as enrollments and instructor.
    
    Recent user commits:
    - chore: Update console log message in main.ts
    - chore: Update npm dependencies to latest versions
    - feat: Add course tag entity interface
    - feat: Add coupon entity interface
    - feat: Add quiz question entity interface
    - feat: Add properties to lesson entity interface
    - feat: Add instructor detail entity interface
    - feat: Add course feedback entity interface
    - feat: Add course certificate entity interface
    - feat: Add category entity interface

---
+++ src/entities/course.entity.ts
@@ -0,0 +1,39 @@
+import { Column, Entity, Index, ManyToOne, OneToMany } from 'typeorm';
+import { BaseEntity } from './bases';
+import { ICourse } from './interfaces';
+import { EnrollmentEntity } from './enrollment.entity';
+import { AccountEntity } from './account.entity';
+
+@Entity({ name: 'course' })
+export class CourseEntity extends BaseEntity<CourseEntity> implements ICourse {
+   @Index('IDX_COURSE_TITLE')
+   @Column({ type: 'varchar', default: '', name: 'title' })
+   title: string;
+
+   @Column({ type: 'varchar', default: '', name: 'description' })
+   description: string;
+
+   @Column({ array: true, type: 'text', name: 'language' })
+   language: string[];
+
+   @Column({ type: 'numeric', default: 0, name: 'price' })
+   price: number;
+
+   @Column({ type: 'varchar', default: '', name: 'level' })
+   level: string;
+
+   @Column({ type: 'numeric', default: 0, name: 'discount' })
+   discount: number;
+
+   @Column({ type: 'varchar', default: '', name: 'category' })
+   category: string;
+
+   @Column({ type: 'varchar', default: '', name: 'thunbnailUrl' })
+   thunbnailUrl: string;
+
+   @OneToMany(() => EnrollmentEntity, (enrollment) => enrollment.course)
+   enrollments: EnrollmentEntity[];
+
+   @ManyToOne(() => AccountEntity, (account) => account.courses)
+   instructor: AccountEntity;
+}
