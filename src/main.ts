449f8720c5c9d5d04d3297d9e4645e8b90ea2fe6
daenv <daenvle69@gmail.com>
2024-05-30 22:28:25 +0700

    chore: Update console log message in main.ts

--- src/main.ts
+++ src/main.ts
@@ -30,7 +30,7 @@ async function bootstrap() {
    const configService = app.get(ConfigService);
    const port = configService.get<string>('PORT') || 3000;
    await app.listen(port, () => {
-      console.log('Listening at http://localhost:' + port + '/' + globalPrefix);
+      console.log('Listening at http://devbulls.us.local2');
    });
 
 }
