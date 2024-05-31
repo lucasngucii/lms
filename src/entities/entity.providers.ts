c24b0e391da1aef1953d094202600137cf1a9a95
daenv <daenvle69@gmail.com>
2024-05-31 23:54:02 +0700

    refactor: Remove unused entity files

--- src/entities/entity.providers.ts
+++ src/entities/entity.providers.ts
@@ -1,16 +1,15 @@
-import { AccountEntity } from './account.entity';
-import { CourseContentEntity } from './courses/content.entity';
-import { CourseEntity } from './courses/course.entity';
-import { DetailAccountEntity } from './detailAccount.entity';
-import { EnrollmentEntity } from './enrollment.entity';
-import { KeyTokenEntity } from './keyToken.entity';
-import { LoginHistoryEntity } from './loginHistory.entity';
-import { CourseModuleEntity } from './courses/module.entity';
-import { ProgresDetailsEntity } from './courses/progresDetail.entity';
-import { RoleEntity } from './role.entity';
-import { SessionEntity } from './session.entity';
-import { CategoryEntity } from './courses/category.entity';
-import { CourseProgressEntity } from './courses/progres.entity';
+import { AccountEntity, DetailAccountEntity } from './accounts';
+import { KeyTokenEntity, LoginHistoryEntity, RoleEntity, SessionEntity } from './auth';
+import {
+   CategoryEntity,
+   CourseContentEntity,
+   CourseEntity,
+   CourseProgressEntity,
+   EnrollmentEntity,
+   ProgresDetailsEntity,
+   CourseModuleEntity,
+} from './courses';
+import {} from './courses/module.entity';
 
 export const entities = [
    AccountEntity,
