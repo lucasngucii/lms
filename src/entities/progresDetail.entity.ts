a9f8bb9df8f06a56d2264aa15a6d85036e73a59c
daenv <daenvle69@gmail.com>
2024-05-31 15:50:27 +0700

    feat: Add ProgresDetailsEntity
    This commit adds the ProgresDetailsEntity class, which represents the details of a course progress. It includes columns for content type, completion status, and a many-to-one relationship with the CourseProgressEntity class.
    
    Recent user commits:
    - feat: Add CategoryEntity
    - refactor: Update gender property in IDetailAccount interface to use EGender enum
    - refactor: Update language property in ICourse interface to be an array
    - feat: Add base.entity.ts
    - feat: Add SessionEntity
    - feat: Add RoleEntity
    - feat: Add LoginHistoryEntity
    - feat: Add KeyTokenEntity
    - feat: Add entity providers
    - feat: Add EnrollmentEntity
    - refactor: Define one-to-many relationship between AccountEntity and LoginHistoryEntity
    - feat: Add DetailAccountEntity
    - feat: Add CourseEntity

---
+++ src/entities/progresDetail.entity.ts
@@ -0,0 +1,19 @@
+import { Column, Entity, ManyToOne } from 'typeorm';
+import { BaseEntity } from './bases';
+import { IProgressDetailEntity } from './interfaces';
+import { CourseProgressEntity } from './progres.entity';
+
+@Entity({ name: 'course_progress_detail' })
+export class ProgresDetailsEntity
+   extends BaseEntity<ProgresDetailsEntity>
+   implements IProgressDetailEntity
+{
+   @Column({ type: 'varchar', name: 'content_type' })
+   contentType: string;
+
+   @Column({ type: 'boolean', default: false, name: 'is_completed' })
+   isCompleted: boolean;
+
+   @ManyToOne(() => CourseProgressEntity, (progres) => progres.details)
+   progres: CourseProgressEntity;
+}
