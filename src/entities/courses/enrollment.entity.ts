6ae6adac07482fb5c1cba8b9afc16a10e2255bba
daenv <daenvle69@gmail.com>
2024-05-31 23:56:15 +0700

    feat: Add EnrollmentEntity for managing course enrollments

---
+++ src/entities/courses/enrollment.entity.ts
@@ -0,0 +1,17 @@
+import { CreateDateColumn, Entity, ManyToOne } from 'typeorm';
+import { BaseEntity } from '../bases';
+import { IEnrollmentEntity } from '../interfaces';
+import { AccountEntity } from '../accounts';
+import { CourseEntity } from '../courses';
+
+@Entity({ name: 'enrollment' })
+export class EnrollmentEntity extends BaseEntity<EnrollmentEntity> implements IEnrollmentEntity {
+   @CreateDateColumn({ name: 'enrollment_date' })
+   enrollmentDate: Date;
+
+   @ManyToOne(() => AccountEntity, (account) => account.enrollments)
+   account: AccountEntity;
+
+   @ManyToOne(() => CourseEntity, (course) => course.enrollments)
+   course: CourseEntity;
+}
