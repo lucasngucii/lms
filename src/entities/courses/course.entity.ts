68585805cfabccce329df860e6116e93d4225fe7
daenv <daenvle69@gmail.com>
2024-05-31 23:42:39 +0700

    feat: Add CourseEntity
    This commit adds the CourseEntity class, which represents a course in the application. It includes columns for title, description, language, price, level, discount, category, thumbnail URL, and establishes relationships with other entities such as EnrollmentEntity, AccountEntity, CategoryEntity, CourseProgressEntity, CourseContentEntity, and CourseModuleEntity.
    
    Recent user commits:
    - feat: Add CourseContentEntity and establish relationship with CourseEntity
    - feat: Add progress details to CourseProgressEntity
    - feat: Add details property to CourseProgressEntity
    - feat: Add CourseContentEntity
    - feat: Add ProgresDetailsEntity
    - feat: Add CourseContentEntity
    - feat: Add ProgresDetailsEntity
    - feat: Add CourseProgressEntity

---
+++ src/entities/courses/course.entity.ts
@@ -0,0 +1,57 @@
+import { Column, Entity, Index, JoinTable, ManyToMany, ManyToOne, OneToMany } from 'typeorm';
+import { BaseEntity } from '../bases';
+import { ICourse } from '../interfaces';
+import { EnrollmentEntity } from '../enrollment.entity';
+import { AccountEntity } from '../account.entity';
+
+import { CourseContentEntity } from './content.entity';
+import { CourseModuleEntity } from './module.entity';
+import { CategoryEntity } from './category.entity';
+import { CourseProgressEntity } from './progres.entity';
+
+@Entity({ name: 'course' })
+export class CourseEntity extends BaseEntity<CourseEntity> implements ICourse {
+   @Index('IDX_COURSE_TITLE')
+   @Column({ type: 'varchar', default: '', name: 'title' })
+   title: string;
+
+   @Column({ type: 'varchar', default: '', name: 'description' })
+   description: string;
+
+   @Column({ array: true, type: 'text', name: 'language' })
+   language: string[];
+
+   @Column({ type: 'numeric', default: 0, name: 'price' })
+   price: number;
+
+   @Column({ type: 'varchar', default: '', name: 'level' })
+   level: string;
+
+   @Column({ type: 'numeric', default: 0, name: 'discount' })
+   discount: number;
+
+   @Column({ type: 'varchar', default: '', name: 'category' })
+   category: string;
+
+   @Column({ type: 'varchar', default: '', name: 'thunbnailUrl' })
+   thunbnailUrl: string;
+
+   @OneToMany(() => EnrollmentEntity, (enrollment) => enrollment.course)
+   enrollments: EnrollmentEntity[];
+
+   @ManyToOne(() => AccountEntity, (account) => account.courses)
+   instructor: AccountEntity;
+
+   @ManyToMany(() => CategoryEntity, { cascade: true })
+   @JoinTable()
+   categories: CategoryEntity[];
+
+   @OneToMany(() => CourseProgressEntity, (progres) => progres.course)
+   progress: CourseProgressEntity;
+
+   @OneToMany(() => CourseContentEntity, (content) => content.course)
+   contents: CourseContentEntity[];
+
+   @OneToMany(() => CourseModuleEntity, (module) => module.course)
+   modules: CourseModuleEntity[];
+}
