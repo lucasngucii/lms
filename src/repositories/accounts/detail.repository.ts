e540f9837a1e21cbeb41a2010ec21013ada04b12
daenv <daenvle69@gmail.com>
2024-06-02 01:32:04 +0700

    feat: Add DetailRepository for managing detail accounts
    This commit adds the `DetailRepository` file to the `src/repositories/accounts` directory. The repository is responsible for managing detail accounts using the `DetailAccountEntity` model. This change enhances the functionality of the application by providing the necessary data access layer for detail account management.

---
+++ src/repositories/accounts/detail.repository.ts
@@ -0,0 +1,4 @@
+import { DetailAccountEntity } from 'src/entities/accounts';
+import { BaseRepository } from '../base';
+
+export class DetailRepository extends BaseRepository<DetailAccountEntity> {}
