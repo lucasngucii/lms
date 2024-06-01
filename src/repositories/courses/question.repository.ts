04d0584590c133ca2a0eacde695d8880553fc28e
daenv <daenvle69@gmail.com>
2024-06-02 01:33:33 +0700

    feat: Add QuizQuestionRepository for managing quiz questions

---
+++ src/repositories/courses/question.repository.ts
@@ -0,0 +1,4 @@
+import { QuizQuestionEntity } from 'src/entities/courses';
+import { BaseRepository } from '../base';
+
+export class QuizQuestionRepository extends BaseRepository<QuizQuestionEntity> {}
