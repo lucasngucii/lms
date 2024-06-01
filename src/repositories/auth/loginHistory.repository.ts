61f64c7c13c42033c8bc117ad6b837bc461d6502
daenv <daenvle69@gmail.com>
2024-06-02 01:32:16 +0700

    feat: Add LoginHistoryRepository for managing login history
    This commit adds the `LoginHistoryRepository` file to the `src/repositories/auth` directory. The repository is responsible for managing login history using the `LoginHistoryEntity` model. This change enhances the functionality of the application by providing the necessary data access layer for login history management.

---
+++ src/repositories/auth/loginHistory.repository.ts
@@ -0,0 +1,4 @@
+import { LoginHistoryEntity } from 'src/entities/auth';
+import { BaseRepository } from '../base';
+
+export class LoginHistoryRepository extends BaseRepository<LoginHistoryEntity> {}
