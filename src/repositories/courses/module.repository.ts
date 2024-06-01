e4789c2850a7bd40ad9de5a72b9b3a774157f755
daenv <daenvle69@gmail.com>
2024-06-02 01:33:17 +0700

    feat: Add CourseModuleRepository for managing course modules
    This commit adds the `CourseModuleRepository` file to the `src/repositories/courses` directory. The repository is responsible for managing course modules using the `CourseModuleEntity` model. This change enhances the functionality of the application by providing the necessary data access layer for course module management.

---
+++ src/repositories/courses/module.repository.ts
@@ -0,0 +1,4 @@
+import { CourseModuleEntity } from 'src/entities/courses';
+import { BaseRepository } from '../base';
+
+export class CourseModuleRepository extends BaseRepository<CourseModuleEntity> {}
