03be1e0d329ff0830254841cdd9d9e19db0339e1
daenv <daenvle69@gmail.com>
2024-06-02 01:31:08 +0700

    feat: Add checkUsername util function to AuthService register method

--- src/apps/auth/auth.service.ts
+++ src/apps/auth/auth.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { MessageResponse } from 'src/common/responses';
 import { RegisterDto } from './dtos';
+import checkUsername from 'src/utils/check-username.util';
 
 @Injectable()
 export class AuthService {
@@ -9,12 +10,24 @@ export class AuthService {
     */
    constructor() {}
 
-   public async register(registerDto: RegisterDto): Promise<MessageResponse> {
-      
-         return {
+//    private async findAccountByUsername(username: string): boolean {
+//       try {
+         
+//       } catch (error) {
+         
+//       }
+//    }
+
+   private async createAccount(registerDto: RegisterDto): Promise<MessageResponse> {
+      return {
          success: true,
          data: registerDto,
          message: 'User registered successfully',
       };
+   }
+
+   public async register(registerDto: RegisterDto): Promise<MessageResponse> {
+      checkUsername(registerDto.username);
+      return await this.createAccount(registerDto);
    }
 }
