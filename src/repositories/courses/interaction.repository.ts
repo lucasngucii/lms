8c9608c756a8cfb70bf8ab6b5e96823345f9b2f2
daenv <daenvle69@gmail.com>
2024-06-02 01:33:06 +0700

    feat: Add InteractionRepository for managing interactions

---
+++ src/repositories/courses/interaction.repository.ts
@@ -0,0 +1,4 @@
+import { InteractionEntity } from 'src/entities/courses';
+import { BaseRepository } from '../base';
+
+export class InteractionRepository extends BaseRepository<InteractionEntity> {}
