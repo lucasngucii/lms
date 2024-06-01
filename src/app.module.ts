a00711d4c0bdb781e932e2a80587c37e097765f8
daenv <daenvle69@gmail.com>
2024-06-01 23:46:21 +0700

    feat: Add AuthModule to app imports
    This commit adds the AuthModule to the list of imports in the AppModule. The AuthModule is necessary for managing authentication in the application.

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,13 +3,15 @@ import { ConfigModule } from '@nestjs/config';
 import * as Joi from 'joi';
 import { LoggerMiddleware } from './middlewares/Logger.middleware';
 import { PostgresDatabaseModule } from './common/databases/postgres/postgres.database';
+import { AuthModule } from './apps/auth/auth.module';
 
 @Module({
    imports: [
       ConfigModule.forRoot({
          isGlobal: true,
          validationSchema: Joi.object({
-            NODE_ENV: Joi.string().valid('development', 'production', 'test', 'provision')
+            NODE_ENV: Joi.string()
+               .valid('development', 'production', 'test', 'provision')
                .default('development'),
             PORT: Joi.number().port().default(3000),
          }),
@@ -19,14 +21,13 @@ import { PostgresDatabaseModule } from './common/databases/postgres/postgres.dat
          },
       }),
       PostgresDatabaseModule,
+      AuthModule,
    ],
    controllers: [],
    providers: [],
 })
 export class AppModule implements NestModule {
    configure(consumer: MiddlewareConsumer) {
-      consumer
-         .apply(LoggerMiddleware)
-         .forRoutes('*');
+      consumer.apply(LoggerMiddleware).forRoutes('*');
    }
 }
