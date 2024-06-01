39c00eab48b33a40fe6be1709c09de7672e66991
daenv <daenvle69@gmail.com>
2024-06-02 01:31:04 +0700

    refactor: Update import statement for PostgresDatabaseModule in app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,7 +2,7 @@ import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import * as Joi from 'joi';
 import { LoggerMiddleware } from './middlewares/Logger.middleware';
-import { PostgresDatabaseModule } from './common/databases/postgres/postgres.database';
+import { PostgresDatabaseModule } from './common/databases/postgres/data.module';
 import { AuthModule } from './apps/auth/auth.module';
 
 @Module({
