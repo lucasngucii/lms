8fdf87d94fdb3b0674655b150b0a98b850186f7e
daenv <daenvle69@gmail.com>
2024-06-01 19:05:47 +0700

    feat: Add OneToMany relationship between InteractionEntity and CourseEntity
    This commit adds a OneToMany relationship between the InteractionEntity and CourseEntity. This relationship allows a course to have multiple interactions. This change is necessary for managing course interactions in the application.

--- src/entities/courses/interaction.entity.ts
+++ src/entities/courses/interaction.entity.ts
@@ -1,6 +1,7 @@
-import { Column, Entity } from 'typeorm';
+import { Column, Entity, OneToMany } from 'typeorm';
 import { BaseEntity } from '../bases';
 import { IInteractionEntity } from '../interfaces';
+import { CourseEntity } from './course.entity';
 
 @Entity({ name: 'course_interaction' })
 export class InteractionEntity extends BaseEntity<InteractionEntity> implements IInteractionEntity {
@@ -9,4 +10,7 @@ export class InteractionEntity extends BaseEntity<InteractionEntity> implements 
 
    @Column({ type: 'varchar', name: 'interaction_content' })
    interactionDate: Date;
+
+   @OneToMany(() => CourseEntity, (course) => course.interaction)
+   course: CourseEntity[];
 }
