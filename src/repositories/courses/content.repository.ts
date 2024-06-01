5f384a43d41faa8c72d2682ca7110b2260961c4c
daenv <daenvle69@gmail.com>
2024-06-02 01:32:50 +0700

    feat: Add CourseContentRepository for managing course content
    This commit adds the `CourseContentRepository` file to the `src/repositories/courses` directory. The repository is responsible for managing course content using the `CourseContentEntity` model. This change enhances the functionality of the application by providing the necessary data access layer for course content management.

---
+++ src/repositories/courses/content.repository.ts
@@ -0,0 +1,4 @@
+import { CourseContentEntity } from 'src/entities/courses';
+import { BaseRepository } from '../base';
+
+export class CourseContentRepository extends BaseRepository<CourseContentEntity> {}
