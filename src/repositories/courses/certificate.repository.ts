3cb694186b0a180e2354b68a312f2b29f5f64f5e
daenv <daenvle69@gmail.com>
2024-06-02 01:32:45 +0700

    feat: Add CertificateRepository for managing certificates
    This commit adds the `CertificateRepository` file to the `src/repositories/courses` directory. The repository is responsible for managing certificates using the `CertificateEntity` model. This change enhances the functionality of the application by providing the necessary data access layer for certificate management.

---
+++ src/repositories/courses/certificate.repository.ts
@@ -0,0 +1,4 @@
+import { CertificateEntity } from 'src/entities/courses';
+import { BaseRepository } from '../base';
+
+export class CertificateRepository extends BaseRepository<CertificateEntity> {}
