3f6229d790feafcb2f581ccd0f1f8b111ff55b77
daenv <daenvle69@gmail.com>
2024-06-02 15:52:01 +0700

    feat: Add AccountEntity and cache module to auth module
    This commit adds the `AccountEntity` to the `auth.module.ts` file in the `src/apps/auth` directory. It also imports and configures the `CacheModule` from `@nestjs/cache-manager` with a time-to-live (TTL) of 5 seconds and a maximum of 10 items. This change enhances the functionality of the application by incorporating the `AccountEntity` model and enabling caching for improved performance.

--- src/apps/auth/auth.module.ts
+++ src/apps/auth/auth.module.ts
@@ -2,10 +2,19 @@ import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { entities } from 'src/entities/entity.providers';
+
+import { AccountEntity } from 'src/entities/accounts';
+import { CacheModule } from '@nestjs/cache-manager';
 
 @Module({
-   imports: [TypeOrmModule.forFeature(entities)],
+   imports: [
+      TypeOrmModule.forFeature([AccountEntity]),
+      CacheModule.register({
+         ttl: 5,
+         max: 10,
+         isGlobal: true,
+      }),
+   ],
    providers: [AuthService],
    controllers: [AuthController],
 })
