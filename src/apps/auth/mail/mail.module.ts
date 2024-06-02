9913fe293d164a7b4e0d94d6b7bd42ea05fb1c54
daenv <daenvle69@gmail.com>
2024-06-02 17:30:45 +0700

    refactor: Remove commented code for MailerModule in mail.module.ts

--- src/apps/auth/mail/mail.module.ts
+++ src/apps/auth/mail/mail.module.ts
@@ -1,9 +1,9 @@
 import { Module } from '@nestjs/common';
-import { MailerModule } from '@nestjs-modules/mailer';
+// import { MailerModule } from '@nestjs-modules/mailer';
 import { MailService } from './mail.service';
 
 @Module({
-   imports: [MailerModule.forRootAsync({})],
+  //  imports: [MailerModule.forRootAsync({})],
    providers: [MailService],
    exports: [MailService],
 })
