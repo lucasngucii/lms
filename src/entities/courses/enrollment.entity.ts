da078095670a643b93923e17434ee46a044ae7fd
daenv <daenvle69@gmail.com>
2024-06-02 17:30:48 +0700

    refactor: Update EnrollmentEntity class formatting

--- src/entities/courses/enrollment.entity.ts
+++ src/entities/courses/enrollment.entity.ts
@@ -6,12 +6,12 @@ import { CourseEntity } from '../courses';
 
 @Entity({ name: 'enrollment' })
 export class EnrollmentEntity extends BaseEntity<EnrollmentEntity> implements IEnrollmentEntity {
-   @CreateDateColumn({ name: 'enrollment_date' })
-   enrollmentDate: Date;
+    @CreateDateColumn({ name: 'enrollment_date' })
+    enrollmentDate: Date;
 
-   @ManyToOne(() => AccountEntity, (account) => account.enrollments)
-   account: AccountEntity;
+    @ManyToOne(() => AccountEntity, (account) => account.enrollments)
+    account: AccountEntity;
 
-   @ManyToOne(() => CourseEntity, (course) => course.enrollments)
-   course: CourseEntity;
+    @ManyToOne(() => CourseEntity, (course) => course.enrollments)
+    course: CourseEntity;
 }
