a3ab3c585610872eba4d02acc35e88244712d174
daenv <daenvle69@gmail.com>
2024-06-03 14:10:42 +0700

    refactor: Remove unused code in AuthController

--- src/apps/auth/auth.controller.ts
+++ src/apps/auth/auth.controller.ts
@@ -5,10 +5,8 @@ import * as _ from 'underscore';
 
 @Controller('auth')
 export class AuthController {
-   /**
-    *
-    */
    constructor(private readonly authService: AuthService) {}
+
    @Post('register')
    public async register(@Body() registerDto: RegisterDto): Promise<unknown> {
       const res = await this.authService.register(registerDto);
