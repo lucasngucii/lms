9fb331c36c1da470131798c73350b3c8c17f3b20
daenv <daenvle69@gmail.com>
2024-06-03 14:10:46 +0700

    feat: Add registration functionality to AuthService

--- src/apps/auth/auth.service.ts
+++ src/apps/auth/auth.service.ts
@@ -1,33 +1,39 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import * as bcrypt from 'bcrypt';
+import { Response } from 'express';
+import { EntityManager } from 'typeorm';
+import { HttpCode } from 'src/constants';
+import { CustomException } from 'src/common';
 import { MessageResponse } from 'src/common/responses';
 import { RegisterDto } from './dtos';
 import checkUsername from 'src/utils/check-username.util';
-import { InjectRepository } from '@nestjs/typeorm';
 import { AccountEntity } from 'src/entities/accounts';
 import { AccountRepository } from 'src/repositories/accounts';
-import { CustomException } from 'src/common';
-import * as bcrypt from 'bcrypt';
-import { EntityManager } from 'typeorm';
+import { setExpireAt } from '../../utils/setExpire.util';
 
 @Injectable()
 export class AuthService {
-   /**
-    *
-    */
    constructor(
       @InjectRepository(AccountEntity) private readonly accountRepository: AccountRepository,
       private readonly entityManager: EntityManager,
-   ) {}
+   ) {
+   }
+
+
+   public async register(registerDto: RegisterDto): Promise<MessageResponse> {
+      checkUsername(registerDto.username);
+      return await this.createAccount(registerDto);
+   }
 
    private async findAccountByUsername(username: string): Promise<AccountEntity> {
       try {
          const foundAccount = await this.accountRepository.findOne({
             where: { username: username },
          });
-
          return foundAccount;
       } catch (error) {
-         throw new CustomException('Account not found', 404);
+         throw new CustomException('', HttpCode.NOT_IMPLEMENTED, error);
       }
    }
 
@@ -52,9 +58,6 @@ export class AuthService {
       });
 
       const saveAccount = await this.entityManager.save(newAccount);
-
-      // send Email notification
-
       return {
          success: true,
          data: {
@@ -64,8 +67,12 @@ export class AuthService {
       };
    }
 
-   public async register(registerDto: RegisterDto): Promise<MessageResponse> {
-      checkUsername(registerDto.username);
-      return await this.createAccount(registerDto);
+   private async setAccessToken(res: Response, access: string): Promise<void> {
+      const expires = setExpireAt(3);
+      res.cookie('access_token', access, {
+         expires: expires,
+         httpOnly: true,
+         secure: true,
+      });
    }
 }
