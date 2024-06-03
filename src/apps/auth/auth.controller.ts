d9f9a63298419f50e9c6e31c380c4d13c5e27f7e
daenv <daenvle69@gmail.com>
2024-06-03 16:13:12 +0700

    refactor: Update AuthController to use MessageResponse in register and login methods

--- src/apps/auth/auth.controller.ts
+++ src/apps/auth/auth.controller.ts
@@ -1,15 +1,21 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, Headers, Res } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { RegisterDto } from './dtos';
-import * as _ from 'underscore';
+import { MessageResponse } from '../../common';
+import { LoginDto } from './dtos/login.dto';
+import { Response } from 'express';
 
 @Controller('auth')
 export class AuthController {
    constructor(private readonly authService: AuthService) {}
 
    @Post('register')
-   public async register(@Body() registerDto: RegisterDto): Promise<unknown> {
-      const res = await this.authService.register(registerDto);
-      return _.omit(res, 'password');
+   public async register(@Body() registerDto: RegisterDto): Promise<MessageResponse> {
+      return await this.authService.register(registerDto);
+   }
+
+   @Post('login')
+   public async login(@Body() loginDto: LoginDto): Promise<MessageResponse> {
+      return await this.authService.login(loginDto);
    }
 }
