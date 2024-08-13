b6b5720e8503374f4502aa0b3d3b9801fe2c9162
Lucas Alehs <lucasalehwork@gmail.com>
2024-08-13 16:33:13 +0700

    refactor: Add new entities and interfaces for various modules

--- src/entities/provider.entity.ts
+++ src/entities/provider.entity.ts
@@ -1,3 +1,43 @@
 import { AccountEntity } from './account.entity';
+import { AssignmentEntity } from './assignment.entity';
+import { CategoryEntity } from './category.entity';
+import { CertificateEntity } from './certificate.entity';
+import { CourseContentEntity } from './content.entity';
+import { CourseEntity } from './course.entity';
+import { EnrollmentEntity } from './enrollment.entity';
+import { InteractionEntity } from './interaction.entity';
+import { KeyTokenEntity } from './key-token.entity';
+import { LessonTestEntity } from './lesson-test.entity';
+import { CourseLessonEntity } from './lesson.entity';
+import { LoginHistoryEntity } from './login.entity';
+import { ModuleEntity } from './module.entity';
+import { ProgresDetailsEntity } from './progres-detail.entity';
+import { ProgressEntity } from './progress.entity';
+import { QuizQuestionEntity } from './question.entity';
+import { ReviewEntity } from './review.entity';
+import { SubmissionEntity } from './submission.entity';
+import { TagEntity } from './tag.entity';
+import { TypeUploadEntity } from './type-upload.entity';
 
-export const entities = [AccountEntity];
+export const entities = [
+   AccountEntity,
+   AssignmentEntity,
+   CategoryEntity,
+   CertificateEntity,
+   CourseContentEntity,
+   EnrollmentEntity,
+   InteractionEntity,
+   KeyTokenEntity,
+   LessonTestEntity,
+   CourseLessonEntity,
+   LoginHistoryEntity,
+   CourseEntity,
+   ModuleEntity,
+   ProgresDetailsEntity,
+   ProgressEntity,
+   QuizQuestionEntity,
+   ReviewEntity,
+   SubmissionEntity,
+   TagEntity,
+   TypeUploadEntity,
+];
